f4769fa8a1d381814a241f3a65e9457f645f7bd2
Raisess <raisechif@gmail.com>
2021-08-08 10:49:44 -0300

    Improved music command.

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import MusicCommand from "./commands/MusicCommand";
 import DiceCommand from "./commands/DiceCommand";
 
 const bot: Core = new Core("sarigue", "?", {
-    commands: [new MusicCommand(), new DiceCommand()],
-    events: [],
-    middlewares: [],
+  commands: [new MusicCommand(), new DiceCommand()],
+  events: [],
+  middlewares: [],
 });
 
 bot.authClient(process.env.TOKEN!);
